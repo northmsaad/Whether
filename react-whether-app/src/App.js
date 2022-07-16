9be79cf9417637979e4f6edb4bcab659bf2d876c
northmsaad <msddr010@gmail.com>
2022-07-16 17:16:59 +0500

    github

---
+++ react-whether-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Search from './components/search/search';
+function App() {
+  const handleonSearchChange = (searchData) => {
+    console.log(searchData)
+  }
+  return (
+    <div className="container">
+      <Search onSearchChange={handleonSearchChange} />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ react-whether-app/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '265dab8fb9msh606a8818ec93e2ep1997e7jsnc4976d00f444',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
